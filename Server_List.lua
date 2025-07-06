25a1a9b33743d49f50045e9973474dba0b94c739
EdeoLex-43 <2436997066@qq.com>
2025-07-06 16:31:08 +0800

    Create Server_List.lua

---
+++ Server_List.lua
@@ -0,0 +1,7 @@
+local Server = {
+    ["被遗弃"] = "https://raw.githubusercontent.com/EdeoLex-43/Main/refs/heads/Script/%E8%A2%AB%E9%81%97%E5%BC%83.lua",
+} -- ["Server Name"] = "Server Url",
+local Other = {
+    ["通用"] = "",
+} -- ["Server Name"] = "Server Url",
+return Server, Other

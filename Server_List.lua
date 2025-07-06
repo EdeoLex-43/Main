34ac0329c8d2863ee6a01d12e0a3879e52baa731
renlua <141826500+renlua@users.noreply.github.com>
2025-07-06 19:18:32 +0800

    Update Server_List.lua

--- Server_List.lua
+++ Server_List.lua
@@ -2,6 +2,6 @@ local Server = {
     ["被遗弃"] = "https://raw.githubusercontent.com/EdeoLex-43/Main/refs/heads/Script/%E8%A2%AB%E9%81%97%E5%BC%83.lua",
 } -- ["Server Name"] = "Server Url",
 local Other = {
-    ["通用"] = "",
+    ["网易云音乐"] = "https://raw.githubusercontent.com/EdeoLex-43/Main/refs/heads/Script/%E7%BD%91%E6%98%93%E4%BA%91.lua",
 } -- ["Server Name"] = "Server Url",
 return Server, Other

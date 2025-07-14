ef2557e51d2b2ccafc6ba44a064c4ff273dee830
renlua <141826500+renlua@users.noreply.github.com>
2025-07-14 19:37:43 +0800

    Update Server_List.lua

--- Server_List.lua
+++ Server_List.lua
@@ -7,6 +7,7 @@
     ["终极战场"] = "https://raw.githubusercontent.com/EdeoLex-43/Main/refs/heads/Script/%E7%BB%88%E6%9E%81%E6%88%98%E5%9C%BA.lua",
     ["生存七天"] = "https://raw.githubusercontent.com/EdeoLex-43/Main/refs/heads/Script/%E7%94%9F%E5%AD%98%E4%B8%83%E5%A4%A9.lua",
     ["柔术无限"] = "https://raw.githubusercontent.com/EdeoLex-43/Main/refs/heads/Script/%E6%9F%94%E6%9C%AF%E6%97%A0%E9%99%90.lua",
+    ["汽车经销大亨"] = "https://raw.githubusercontent.com/EdeoLex-43/Main/refs/heads/Script/%E6%B1%BD%E8%BD%A6%E7%BB%8F%E9%94%80%E5%A4%A7%E4%BA%A8.lua",
 } -- ["Server Name"] = "Server Url",
 local Other = {
     ["网易云音乐"] = "https://raw.githubusercontent.com/EdeoLex-43/Main/refs/heads/Script/%E7%BD%91%E6%98%93%E4%BA%91.lua",
